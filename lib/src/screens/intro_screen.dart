4e73f28f2f10ef6094a13b0f69e04126ea6d208a
Prateek Thakur <prateekthakur272@gmail.com>
2024-04-25 10:14:17 +0530

    adds intro screen

--- lib/src/screens/intro_screen.dart
+++ lib/src/screens/intro_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_store/src/extensions/build_context_extension.dart';
+import 'package:video_player/video_player.dart';
 
 class IntroScreen extends StatefulWidget {
   const IntroScreen({super.key});
@@ -8,8 +10,70 @@ class IntroScreen extends StatefulWidget {
 }
 
 class _IntroScreenState extends State<IntroScreen> {
+  late VideoPlayerController _videoPlayerController;
+
+  @override
+  void initState() {
+    _videoPlayerController =
+        VideoPlayerController.asset('assets/videos/intro.mp4')
+          ..initialize().then((_) {
+            _videoPlayerController.play();
+            _videoPlayerController.setLooping(true);
+            setState(() {});
+          });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          SizedBox.expand(
+              child: FittedBox(
+                  fit: BoxFit.cover,
+                  child: SizedBox(
+                      height: _videoPlayerController.value.size.height,
+                      width: _videoPlayerController.value.size.width,
+                      child: VideoPlayer(_videoPlayerController)))),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Grocery Store',
+                textAlign: TextAlign.center,
+                style: context.textTheme.displayLarge!
+                    .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+              Text('Get delivered in minutes in few taps', style: context.textTheme.bodyLarge!.copyWith(
+                color: Colors.white
+              ),)
+            ],
+          ),
+          Positioned(
+            bottom: 56,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                FilledButton(onPressed: (){}, child: const Text('Login Or Register')),
+                const SizedBox(width: 24,),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                  ),
+                    onPressed: (){}, child: const Text('Continue As Guest')),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
   }
 }
